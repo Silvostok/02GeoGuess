62ba723e9c3a2a4c747d281d93fe16d7e2134192
Miguel Angel Rubio Caballero <Miguel.Rubio.Caballero@hva.nl>
2018-10-06 01:32:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/miguelangelrubiocaballero/a02geoguess/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.miguelangelrubiocaballero.a02geoguess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
